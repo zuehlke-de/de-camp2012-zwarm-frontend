577224d02ae5c5e0137d1a99fc21c3fc22f8b21c
wey <wey@zuehlke.com>
2012-05-22 17:17:31 +0200

    updated DatePicker with time, ok and cancel buttons

--- Resources/ui/DatePickerView.js
+++ Resources/ui/DatePickerView.js
@@ -1,22 +1,52 @@
-function DatePickerView(changeHandler) {
+function DatePickerView(callback) {
 	
 	var self = Ti.UI.createWindow({
 		backgroundColor:'#ffffff'
 	});
+	
+	var date = new Date();
+	var time = new Date();
 		
-	var picker = Ti.UI.createPicker({
+	var datePicker = Ti.UI.createPicker({
 	    type:Ti.UI.PICKER_TYPE_DATE,
 	    minDate: new Date(),
 	    maxDate: new Date(2020, 12, 31),
 	    value: new Date(2012, 5, 22)
 	});
 	
-	picker.addEventListener('change', function(e) {
-  		changeHandler(e.value);
-  		self.close();
+	var timePicker = Ti.UI.createPicker({
+	    type:Ti.UI.PICKER_TYPE_TIME,
+	    minDate: new Date(),
+	    maxDate: new Date(2020, 12, 31),
+	    value: new Date()
 	});
 	
-	self.add(picker);
+	var okButton = Ti.UI.createButton({
+		title: "OK"
+	});
+	okButton.addEventListener('click', function() {
+		callback(date, time);
+		self.close();
+	});
+	
+	var cancelButton = Ti.UI.createButton({
+		title: "Cancel"
+	});
+	cancelButton.addEventListener('click', function() {
+		self.close();
+	});
+	
+	datePicker.addEventListener('change', function(e) {
+  		date = e.value;
+	});
+	timePicker.addEventListener('change', function(e) {
+  		time = e.value;
+	});
+	
+	self.add(datePicker);
+	self.add(timePicker);
+	self.add(okButton);
+	self.add(cancelButton);
 	
 	return self;
 	
