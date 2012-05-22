843be494b88c939aa2598961a358569c2f474530
pascalalichls <pascal.alich@gmx.de>
2012-05-22 14:33:59 +0200

    InitView runs without server interaction

--- Resources/app.js
+++ Resources/app.js
@@ -1,37 +1,24 @@
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
+	
+var openInitView = function() {
+	var InitView = require('ui/InitView');
+	var view = new InitView();
+	view.addEventListener('close', function(event) {
+		openMainView();
+	});
+	view.open();
+}
+var openMainView = function() {
+	Ti.API.debug(Ti.App.Properties.getString('user.id') + " " + Ti.App.Properties.getString('user.name'));
+	var MainView = require('ui/TabContainer');
+	var view = new MainView();
+	view.open();
+}
 
-
-//
-// view creation example
-//
-var SampleView = require('ui/SampleView');
-
-var win1 = new SampleView();
-
-var win2 = new SampleView();
-
-
-var tab1 = Titanium.UI.createTab({
-	title: 'MyTitle',
-    window: win1	
-});
-
-var tab2 = Titanium.UI.createTab({
-	title: 'MyTitle2',
-    window: win2	
-});
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+if (!Ti.App.Properties.hasProperty('user.id')) {
+	openInitView();
+} else {
+	openMainView();
+}
--- Resources/ui/InitView.js
+++ Resources/ui/InitView.js
@@ -1,22 +1,67 @@
 function InitView() {
 	
-	//create object instance
+	// create object instance
 	var self = Ti.UI.createWindow({
-		backgroundColor:'#ffffff'
+		title:'Login',
+	    width: '100%',
+	    height: '100%',
+		backgroundColor:'#ffffff',
 	});
 		
-	var label1 = Titanium.UI.createLabel({
-		color:'#000',
-		text:'Tralala',
-		font:{fontSize:20,fontFamily:'Helvetica Neue'},
-		textAlign:'center',
-		width:'auto'
+	var containerView = Ti.UI.createView({
+		backgroundColor: 'transparent',
+		width: '90%',
+		height: '90%',
+		top: '5%',
+		left: '5%',
+		layout: 'vertical'
 	});
+	self.add(containerView);
 	
-	self.add(label1);
+	var logoImage = Titanium.UI.createImageView({
+	  image:'../zwarm-logo.png',
+	  top: '7%'
+	});
+	containerView.add(logoImage);
+	
+	var nicknameLabel = Titanium.UI.createLabel({
+		color:'#000000',
+		text:'Please enter a nickname',
+		font:{fontSize:30, fontFamily:'Helvetica Neue'},
+		textAlign:'center',
+		width:Ti.UI.FILL,
+		height:Ti.UI.SIZE,
+		top: '7%'
+	});
+	containerView.add(nicknameLabel);
+	
+	var nicknameInput = Ti.UI.createTextField({
+		width:Ti.UI.FILL,
+		height:Ti.UI.SIZE,
+		top: '7%',
+		hintText: 'anonymous'
+	});
+	containerView.add(nicknameInput);
+	
+	var initButton = Ti.UI.createButton({
+		title: 'Go!',
+		width: Ti.UI.FILL,
+		top: '7%',
+	});
+	initButton.addEventListener('click', function(event) {
+		var userId = Ti.Platform.createUUID();
+		Ti.App.Properties.setString('user.id', userId);
+		var userName = nicknameInput.value.trim();
+		if (userName.length == 0) {
+			userName = nicknameInput.hintText;
+		}
+		Ti.App.Properties.setString('user.name', userName);
+	    // TODO call server		
+		self.close();
+	});
+	containerView.add(initButton);
 	
 	return self;
-	
 };
 
 module.exports = InitView;
