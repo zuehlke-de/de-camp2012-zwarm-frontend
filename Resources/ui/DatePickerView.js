262b854b2aee55f564b2119ba24727b577491ba8
wey <wey@zuehlke.com>
2012-05-22 17:52:39 +0200

    improved date picker

--- Resources/ui/DatePickerView.js
+++ Resources/ui/DatePickerView.js
@@ -1,7 +1,8 @@
 function DatePickerView(callback) {
 	
 	var self = Ti.UI.createWindow({
-		backgroundColor:'#ffffff'
+		backgroundColor:'#ffffff',
+		layout: 'vertical'
 	});
 	
 	var date = new Date();
@@ -11,18 +12,33 @@ function DatePickerView(callback) {
 	    type:Ti.UI.PICKER_TYPE_DATE,
 	    minDate: new Date(),
 	    maxDate: new Date(2020, 12, 31),
-	    value: new Date(2012, 5, 22)
+	    value: new Date(2012, 5, 22),
+	    top: '20'
+	});
+	datePicker.addEventListener('change', function(e) {
+  		date = e.value;
 	});
 	
 	var timePicker = Ti.UI.createPicker({
 	    type:Ti.UI.PICKER_TYPE_TIME,
 	    minDate: new Date(),
 	    maxDate: new Date(2020, 12, 31),
-	    value: new Date()
+	    value: new Date(),
+	    top: '20'
+	});
+	timePicker.addEventListener('change', function(e) {
+  		time = e.value;
+	});
+	
+	var buttonContainer = Ti.UI.createView({
+		layout: 'horizontal',
+		top: '50',
 	});
 	
 	var okButton = Ti.UI.createButton({
-		title: "OK"
+		title: "OK",
+		width: '35%',
+		left: '10%'
 	});
 	okButton.addEventListener('click', function() {
 		callback(date, time);
@@ -30,23 +46,21 @@ function DatePickerView(callback) {
 	});
 	
 	var cancelButton = Ti.UI.createButton({
-		title: "Cancel"
+		title: "Cancel",
+		width: '35%',
+		left: '10%'
 	});
 	cancelButton.addEventListener('click', function() {
 		self.close();
 	});
 	
-	datePicker.addEventListener('change', function(e) {
-  		date = e.value;
-	});
-	timePicker.addEventListener('change', function(e) {
-  		time = e.value;
-	});
+	buttonContainer.add(okButton);
+	buttonContainer.add(cancelButton);
+	
 	
 	self.add(datePicker);
 	self.add(timePicker);
-	self.add(okButton);
-	self.add(cancelButton);
+	self.add(buttonContainer);
 	
 	return self;
 	
