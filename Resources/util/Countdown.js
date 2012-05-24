518c031c60b7b972aa88feb8ce853453ed7dd222
pascalalichls <pascal.alich@gmx.de>
2012-05-24 10:46:57 +0200

    Countdown repaired

--- Resources/util/Countdown.js
+++ Resources/util/Countdown.js
@@ -1,48 +1,41 @@
 // initiate new timer object (licensed MIT. copyright Daniel Tamas http://rborn.info)
-
-        
-        var countDown =  function( m , s, fn_tick, fn_end  ) {
-                return {
-                        total_sec:m*60+s,
-                        timer:this.timer,
-                        set: function(m,s) {
-                                this.total_sec = parseInt(m)*60+parseInt(s);
-                                this.time = {m:m,s:s};
-                                return this;
-                        },
-                        start: function() {
-                                var self = this;
-                                this.timer = setInterval( function() {
-                                        if (self.total_sec) {
-                                                self.total_sec--;
-                                                self.time = { m : parseInt(self.total_sec/60), s: (self.total_sec%60) };
-                                                fn_tick();
-                                        }
-                                        else {
-                                                self.stop();
-                                                fn_end();
-                                        }
-                                        }, 1000 );
-                                return this;
-                        },
-                        stop: function() {
-                                clearInterval(this.timer);
-                                this.time = {m:0,s:0};
-                                this.total_sec = 0;
-                                return this;
-                        }
-                };
-        };    
-        
-        
-        var timeString = function (timer){
-        	if(timer.time.s<10){
-        		return timer.time.m + ":0" + timer.time.s;
-        	}
-        	return timer.time.m+":"+timer.time.s;
-        }  
-        
-
-
-
-
+var CountDown =  function( m , s, fn_tick, fn_end ) {
+        return {
+                total_sec:m*60+s,
+                timer:this.timer,
+                set: function(m,s) {
+                        this.total_sec = parseInt(m)*60+parseInt(s);
+                        this.time = {m:m,s:s};
+                        return this;
+                },
+                start: function() {
+                        var self = this;
+                        this.timer = setInterval( function() {
+                                if (self.total_sec) {
+                                        self.total_sec--;
+                                        self.time = { m : parseInt(self.total_sec/60), s: (self.total_sec%60) };
+                                        fn_tick();
+                                }
+                                else {
+                                        self.stop();
+                                        fn_end();
+                                }
+                                }, 1000 );
+                        return this;
+                },
+                stop: function() {
+                        clearInterval(this.timer);
+                        this.time = {m:0,s:0};
+                        this.total_sec = 0;
+                        return this;
+                },
+                toString: function() {
+                	var tempM = Math.floor(this.total_sec / 60);
+                	var tempS = this.total_sec % 60;
+            		if(tempS<10){
+						return tempM + ":0" + tempS;
+					}
+					return tempM+":"+tempS;
+                }
+        };
+};
